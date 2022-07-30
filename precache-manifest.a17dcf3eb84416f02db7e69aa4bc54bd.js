9872e5347aa67da3aeb40395c33ff8640826ccb2
bkDevMe <bkflying1@163.com>
2022-07-30 18:20:55 +0000

    Deploying to gh-pages from  @ 417be1649173ee9597516b307955d84b36b5e83d 🚀

---
+++ precache-manifest.a17dcf3eb84416f02db7e69aa4bc54bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dffb5450f4bb2fabe9e694f6fb7c5131",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "1bd9f361a5fbbb1dd3f0",
+    "url": "/github-actions-demo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "55c935a2be73a9c55687",
+    "url": "/github-actions-demo/static/js/2.d0dec738.chunk.js"
+  },
+  {
+    "revision": "1bd9f361a5fbbb1dd3f0",
+    "url": "/github-actions-demo/static/js/main.5aad3b8d.chunk.js"
+  },
+  {
+    "revision": "e45d09c88885cc788ce2",
+    "url": "/github-actions-demo/static/js/runtime~main.b3f543ac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-actions-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
